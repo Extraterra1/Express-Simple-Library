b9fbadf6c5a17e17bf35f6d1d4a11db792255b48
Carlos Pires <extraterra1@gmail.com>
2023-12-04 14:38:34 +0000

    bookinstance link

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,11 +1,10 @@
 const BookInstance = require('../models/bookInstanceModel.js');
 const asyncHandler = require('express-async-handler');
-const moment = require('moment');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
   const bookInstances = await BookInstance.find().populate('book').exec();
-  res.render('bookInstanceList', { title: 'Book Instances | Lil Library', bookInstances, moment });
+  res.render('bookInstanceList', { title: 'Book Instances | Lil Library', bookInstances });
 });
 
 // Display detail page for a specific BookInstance.
