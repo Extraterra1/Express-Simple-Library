42dffa0c326dab7da020e0a263bb8f57848bbcc2
Carlos Pires <extraterra1@gmail.com>
2023-12-07 11:44:26 +0000

    update genre post logic

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -79,6 +79,17 @@ exports.genre_update_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-});
+exports.genre_update_post = [
+  // Validate Name Field
+  body('name', 'Genre name must be at least 3 characters long').trim().isLength({ min: 3 }).escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const genre = new Genre({ name: req.body.name, _id: req.params.id });
+
+    if (!errors.isEmpty()) return res.render('createGenre', { genre, errors });
+
+    await Genre.findByIdAndUpdate(req.params.id, genre);
+    res.redirect(genre.url);
+  })
+];
